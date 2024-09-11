0d64bc13be9eb6d9d8d7fde20055d4b31f070780
victorfaruna <farunavictor043@gmail.com>
2024-09-12 00:50:45 +0100

    yes

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -100,7 +100,7 @@
 								>
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										fill="black"
+										fill="currentColor"
 										viewBox="0 0 24 24"
 										stroke-width="1.5"
 										stroke="currentColor"
