3b6d800c3aaa660c948517494f7bf767d98d3c78
victorfaruna <farunavictor043@gmail.com>
2024-09-18 17:45:19 +0100

    oya

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,5 +4,8 @@ import * as schema from './schema';
 import postgres from 'postgres';
 import { config } from 'dotenv';
 config({ path: '.env' });
-const client = postgres(process.env.DATABASE_URL!, { ssl: 'require', port: 6543 });
+const client = postgres(
+	'postgresql://postgres.wjlwisuqqdkcqphjuvjt:nzJNx2FM4lFH0SKb@aws-0-us-east-1.pooler.supabase.com:6543/postgres',
+	{ ssl: 'require', port: 6543 }
+);
 export const db = drizzle(client, { schema });
