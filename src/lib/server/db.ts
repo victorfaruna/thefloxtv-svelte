32768f44d1134c84ab2866e680a50a2ed3e092a2
victorfaruna <farunavictor043@gmail.com>
2024-09-18 17:58:13 +0100

    cook

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,6 @@
-import { dev } from '$app/environment';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
+import { DATABASE_URL } from '$env/static/private';
 import postgres from 'postgres';
-import { config } from 'dotenv';
-config({ path: '.env' });
-const client = postgres(
-	'postgresql://postgres.wjlwisuqqdkcqphjuvjt:nzJNx2FM4lFH0SKb@aws-0-us-east-1.pooler.supabase.com:6543/postgres',
-	{ ssl: 'require', port: 6543 }
-);
+const client = postgres(DATABASE_URL, { ssl: 'require', port: 6543 });
 export const db = drizzle(client, { schema });
--- src/routes/api/trending-tv/+server.ts
+++ src/routes/api/trending-tv/+server.ts
@@ -4,26 +4,26 @@ import { trendingTvTable } from '$src/lib/server/schema';
 
 export async function GET() {
 	try {
-		const trending1 = await fetchTrending('movie', 'day');
+		const trending1 = await fetchTrending('tv', 'day');
 		for (const trending of trending1) {
 			await db.insert(trendingTvTable).values({
 				tmdb_id: trending.id,
 				imdb_id: trending.imdb_id,
-				name: trending.name,
-				overview: trending.overview,
-				popularity: trending.popularity,
-				vote_average: trending.vote_average,
-				vote_count: trending.vote_count,
-				first_air_date: trending.first_air_date,
-				last_air_date: trending.last_air_date,
-				last_season: trending.last_episode_to_air?.season_number,
-				last_episode: trending.last_episode_to_air?.episode_number,
+				name: trending.name ?? '',
+				overview: trending.overview ?? '',
+				popularity: trending.popularity ?? '',
+				vote_average: trending.vote_average ?? '',
+				vote_count: trending.vote_count ?? '',
+				first_air_date: trending.first_air_date ?? '',
+				last_air_date: trending.last_air_date ?? '',
+				last_season: trending.last_episode_to_air?.season_number ?? '',
+				last_episode: trending.last_episode_to_air?.episode_number ?? '',
 				genre1: trending.genres[0]?.name ?? '',
 				genre2: trending.genres[1]?.name ?? '',
 				genre3: trending.genres[2]?.name ?? '',
-				poster_path: trending.poster_path,
-				backdrop_path: trending.backdrop_path,
-				logo_path: trending.images?.logos[0]?.file_path ?? '',
+				poster_path: trending.poster_path ?? '',
+				backdrop_path: trending?.backdrop_path ?? '',
+				logo_path: trending?.images?.logos[0]?.file_path ?? '',
 				media_type: 'tv'
 			});
 		}
