d9f14d749f5e95307c62cf6df9358d791042524b
victorfaruna <farunavictor043@gmail.com>
2024-09-18 16:22:16 +0100

    oya na

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,9 @@
+import { dev } from '$app/environment';
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 config({ path: '.env' });
-const client = postgres(process.env.DATABASE_URL!);
+const client = dev
+	? postgres(process.env.DATABASE_URL!)
+	: postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 export const db = drizzle(client);
