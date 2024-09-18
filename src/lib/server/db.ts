c9d1a88853a1970979c5ebe1892d91e5b1ab917d
victorfaruna <farunavictor043@gmail.com>
2024-09-18 17:43:26 +0100

    yes

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,6 +4,5 @@ import * as schema from './schema';
 import postgres from 'postgres';
 import { config } from 'dotenv';
 config({ path: '.env' });
-const { DATABASE_URL }: any = process.env;
-const client = postgres(DATABASE_URL, { port: 6543 });
+const client = postgres(process.env.DATABASE_URL!, { ssl: 'require', port: 6543 });
 export const db = drizzle(client, { schema });
