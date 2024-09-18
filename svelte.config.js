1e3fe4084bd7f630abf51431c30c153c78730422
victorfaruna <farunavictor043@gmail.com>
2024-09-18 16:09:12 +0100

    alright

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ runtime: 'nodejs20.x' }),
 		alias: {
 			$components: './src/components',
 			$static: './static',
