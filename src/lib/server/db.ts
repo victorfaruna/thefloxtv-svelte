66edda49dfc7d6d2cf891ec008f1c6bd23c669d5
victorfaruna <farunavictor043@gmail.com>
2024-09-19 08:30:25 +0100

    yes now

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,6 +2,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 import { DATABASE_URL } from '$env/static/private';
 import postgres from 'postgres';
-import { dev } from '$app/environment';
-const client = dev ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: true });
+const client = postgres(DATABASE_URL, { ssl: 'require', port: 5432 });
 export const db = drizzle(client, { schema });
