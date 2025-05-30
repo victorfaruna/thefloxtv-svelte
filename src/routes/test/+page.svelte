988811cf435b5bab59343f84475068b2a69369d0
victorfaruna <farunavictor043@gmail.com>
2025-05-30 17:41:26 +0100

    fix: removed buggy vidsrc.to module

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,12 +1,4 @@
-<script>
-	import tmdbScrape from '$lib/vidsrc';
-	async function test() {
-		const result = await tmdbScrape('76479', 'tv', 1, 1);
-		console.log(result);
-	}
-</script>
-
 <div class="container">
 	<h1>Test</h1>
-	<button onclick={test}>Test</button>
+	<button>Test</button>
 </div>
